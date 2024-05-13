0313220814912ea4d3df7f3a6a9d3be7db08c622
Myha Kalaj <mybe2000@gmail.com>
2024-05-13 12:33:03 -0400

    created index.html style.css and script.js for secure-the-vault

---
+++ script.js
@@ -0,0 +1,22 @@
+//pseudocode
+//create an alert to ask the user to input the three codes
+//create prompt for first code number with division operator
+//create prompt for second code number with times operator
+//create prompt for third code number with plus operator
+//crate alert with the combination
+
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+//assign three variables
+const one = 350 / 35;
+const two = 8 * 5;
+const three = 23 + 16;
+
+//prompt user input for each code
+prompt("350 /35");
+prompt("8 *5");
+prompt("23+16");
+
+//display combination to the user
+alert("the combination is" + " " + one + " " + two + " " + three);
